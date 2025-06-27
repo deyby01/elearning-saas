a240cd0e1118b634a04b0e0edcc635d12ef8a7ff
deyby01 <camachodeybygerman@gmail.com>
2025-06-26 20:33:47 -0400

    feat(courses): Add course detail view and template.

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -5,4 +5,5 @@ app_name = 'courses' # Buena practica para nombrar las urls
 
 urlpatterns = [
     path('', views.CourseListView.as_view(), name='course_list'),
+    path('<int:pk>/', views.CourseDetailView.as_view(), name='course_detail'),
 ]
--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Course
 
 
@@ -7,3 +7,9 @@ class CourseListView(ListView):
     model = Course
     template_name = 'courses/course_list.html'
     context_object_name = 'courses' # Nombre que usaremos en la plantilla
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course_detail.html'
+    context_object_name = 'course'
